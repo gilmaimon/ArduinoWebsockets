3adc2ec1810a750984d30e232b4f7b02920cb40a
Gil Maimon <gilm2121@gmail.com>
2019-04-13 19:19:56 +0300

    Changed esp8266 tcp impl to use generic ESP client class.

--- src/tiny_websockets/network/esp8266/esp8266_tcp.hpp
+++ src/tiny_websockets/network/esp8266/esp8266_tcp.hpp
@@ -9,13 +9,14 @@
 #include <ESP8266WiFi.h>
 
 namespace websockets { namespace network {
-  class Esp8266TcpClient : public TcpClient {
+  template <class WifiClientImpl> 
+  class GenericEspTcpClient : public TcpClient {
   public:
-    Esp8266TcpClient(WiFiClient c) : client(c) {
+    GenericEspTcpClient(WifiClientImpl c) : client(c) {
       client.setNoDelay(true);
     }
     
-    Esp8266TcpClient() {}
+    GenericEspTcpClient() {}
 
     bool connect(const WSString& host, const int port) {
       auto didConnect = client.connect(host.c_str(), port);
@@ -63,74 +64,22 @@ namespace websockets { namespace network {
       client.stop();
     }
 
-    virtual ~Esp8266TcpClient() {
+    virtual ~GenericEspTcpClient() {
       client.stop();
     }
 
   protected:
+    WifiClientImpl client;
+
     int getSocket() const override {
       return -1;
-    }
-    
-  private:
-    WiFiClient client;
+    }    
   };
 
-  class SecuredEsp8266TcpClient : public TcpClient {
+  typedef GenericEspTcpClient<WiFiClient> Esp8266TcpClient;
+
+  class SecuredEsp8266TcpClient : public GenericEspTcpClient<WiFiClientSecure> {
   public:
-    SecuredEsp8266TcpClient(WiFiClientSecure c) : client(c) {
-      client.setNoDelay(true);
-    }
-    
-    SecuredEsp8266TcpClient() {
-    }
-
-    bool connect(const WSString& host, const int port) {
-      auto didConnect = client.connect(host.c_str(), port);
-      client.setNoDelay(true);
-      return didConnect;
-    }
-
-    bool poll() {
-      return client.available();
-    }
-
-    bool available() override {
-      return client.connected();
-    }
-
-    void send(const WSString& data) override {
-      client.write(reinterpret_cast<uint8_t*>(const_cast<char*>(data.c_str())), data.size());
-    }
-
-    void send(const WSString&& data) override {
-      client.write(reinterpret_cast<uint8_t*>(const_cast<char*>(data.c_str())), data.size());
-    }
-
-    void send(const uint8_t* data, const uint32_t len) override {
-      client.write(data, len);
-    }
-    
-    WSString readLine() override {
-      int val;
-      WSString line;
-      do {
-        val = client.read();
-        if(val < 0) continue;
-        line += (char)val;
-      } while(val != '\n');
-      if(!available()) close();
-      return line;
-    }
-
-    void read(uint8_t* buffer, const uint32_t len) override {
-      client.read(buffer, len);
-    }
-
-    void close() override {
-      client.stop();
-    }
-
     void setInsecure() {
       client.setInsecure();
     }
@@ -138,18 +87,6 @@ namespace websockets { namespace network {
     void setFingerprint(const char* fingerprint) {
       client.setFingerprint(fingerprint);
     }
-    
-    virtual ~SecuredEsp8266TcpClient() {
-      client.stop();
-    }
-
-  protected:
-    int getSocket() const override {
-      return -1;
-    }
-    
-  private:
-    WiFiClientSecure client;
   };
 
   #define DUMMY_PORT 0
