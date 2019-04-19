4d797dbc17d481814fd54f3afeb73d4cdc778057
Gil Maimon <gilm2121@gmail.com>
2019-04-19 17:47:18 +0300

    Extracted generic esp client template to a seperate file. ESP8266 impl now uses the generic impl

--- src/tiny_websockets/network/esp8266/esp8266_tcp.hpp
+++ src/tiny_websockets/network/esp8266/esp8266_tcp.hpp
@@ -5,89 +5,13 @@
 #include <tiny_websockets/internals/ws_common.hpp>
 #include <tiny_websockets/network/tcp_client.hpp>
 #include <tiny_websockets/network/tcp_server.hpp>
+#include <tiny_websockets/network/generic_esp/generic_esp_clients.hpp>
 
 #include <ESP8266WiFi.h>
 
 namespace websockets { namespace network {
-  template <class WifiClientImpl> 
-  class GenericEspTcpClient : public TcpClient {
-  public:
-    GenericEspTcpClient(WifiClientImpl c) : client(c) {
-      client.setNoDelay(true);
-    }
-    
-    GenericEspTcpClient() {}
-
-    bool connect(const WSString& host, const int port) {
-      auto didConnect = client.connect(host.c_str(), port);
-      client.setNoDelay(true);
-      return didConnect;
-    }
-
-    bool poll() {
-      return client.available();
-    }
-
-    bool available() override {
-      return client.connected();
-    }
-
-    void send(const WSString& data) override {
-      client.write(reinterpret_cast<uint8_t*>(const_cast<char*>(data.c_str())), data.size());
-    }
-
-    void send(const WSString&& data) override {
-      client.write(reinterpret_cast<uint8_t*>(const_cast<char*>(data.c_str())), data.size());
-    }
-
-    void send(const uint8_t* data, const uint32_t len) override {
-      client.write(data, len);
-    }
-    
-    WSString readLine() override {
-      int val;
-      WSString line;
-      do {
-        val = client.read();
-        if(val < 0) continue;
-        line += (char)val;
-      } while(val != '\n');
-      if(!available()) close();
-      return line;
-    }
-
-    void read(uint8_t* buffer, const uint32_t len) override {
-      client.read(buffer, len);
-    }
-
-    void close() override {
-      client.stop();
-    }
-
-    virtual ~GenericEspTcpClient() {
-      client.stop();
-    }
-
-  protected:
-    WifiClientImpl client;
-
-    int getSocket() const override {
-      return -1;
-    }    
-  };
-
   typedef GenericEspTcpClient<WiFiClient> Esp8266TcpClient;
-
-  class SecuredEsp8266TcpClient : public GenericEspTcpClient<WiFiClientSecure> {
-  public:
-    void setInsecure() {
-      client.setInsecure();
-    }
-
-    void setFingerprint(const char* fingerprint) {
-      client.setFingerprint(fingerprint);
-    }
-  };
+  typedef GenericSecuredEspTcpClient<WiFiClientSecure> SecuredEsp8266TcpClient;
 
   #define DUMMY_PORT 0
 
