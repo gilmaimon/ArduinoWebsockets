aeba83d7eefbc675dc8283252ac69ba81dde2af7
Gil Maimon <gilm2121@gmail.com>
2019-04-20 19:06:04 +0300

    Added a secured esp8266 echo example

---
+++ examples/Secured-Esp8266-Client/Secured-Esp8266-Client.ino
@@ -0,0 +1,88 @@
+/*
+	Secured Esp8266 Websockets Client
+
+	This sketch:
+        1. Connects to a WiFi network
+        2. Connects to a Websockets server (using WSS)
+        3. Sends the websockets server a message ("Hello Server")
+        4. Sends the websocket server a "ping"
+        5. Prints all incoming messages while the connection is open
+
+    NOTE:
+    The sketch dosen't check or indicates errors while connection to 
+    WiFi or to the websockets server. For full example you might want 
+    to try the example named "Esp8266-Client".
+
+	Hardware:
+        For this sketch you only need an ESP8266 board.
+
+	Created 15/02/2019
+	By Gil Maimon
+	https://github.com/gilmaimon/ArduinoWebsockets
+
+*/
+
+#include <ArduinoWebsockets.h>
+#include <ESP8266WiFi.h>
+
+const char* ssid = "ssid"; //Enter SSID
+const char* password = "password"; //Enter Password
+
+const char* websockets_connection_string = "wss://echo.websocket.org/"; //Enter server adress
+
+// This fingerprint was updated 20.04.2019
+const char echo_org_ssl_fingerprint[] PROGMEM = "D5 07 4D 79 B2 D2 53 D7 74 E6 1B 46 C5 86 4E FE AD 00 F1 98";
+
+using namespace websockets;
+
+void onMessageCallback(WebsocketsMessage message) {
+    Serial.print("Got Message: ");
+    Serial.println(message.data());
+}
+
+void onEventsCallback(WebsocketsEvent event, String data) {
+    if(event == WebsocketsEvent::ConnectionOpened) {
+        Serial.println("Connnection Opened");
+    } else if(event == WebsocketsEvent::ConnectionClosed) {
+        Serial.println("Connnection Closed");
+    } else if(event == WebsocketsEvent::GotPing) {
+        Serial.println("Got a Ping!");
+    } else if(event == WebsocketsEvent::GotPong) {
+        Serial.println("Got a Pong!");
+    }
+}
+
+WebsocketsClient client;
+void setup() {
+    Serial.begin(115200);
+    // Connect to wifi
+    WiFi.begin(ssid, password);
+
+    // Wait some time to connect to wifi
+    for(int i = 0; i < 10 && WiFi.status() != WL_CONNECTED; i++) {
+        Serial.print(".");
+        delay(1000);
+    }
+
+    // run callback when messages are received
+    client.onMessage(onMessageCallback);
+    
+    // run callback when events are occuring
+    client.onEvent(onEventsCallback);
+
+    // Before connecting, set the ssl fingerprint of the server
+    client.setFingerprint(echo_org_ssl_fingerprint);
+
+    // Connect to server
+    client.connect(websockets_connection_string);
+
+    // Send a message
+    client.send("Hello Server");
+
+    // Send a ping
+    client.ping();
+}
+
+void loop() {
+    client.poll();
+}
