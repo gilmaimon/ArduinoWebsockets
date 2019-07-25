95841a97644a97233889bda4455caa4bc8861575
Gil Maimon <gilm2121@gmail.com>
2019-07-25 15:17:30 +0300

    Ported changes form tinywebsocket

--- src/tiny_websockets/network/tcp_client.hpp
+++ src/tiny_websockets/network/tcp_client.hpp
@@ -10,7 +10,7 @@ namespace websockets { namespace network {
     virtual void send(const WSString&& data) = 0;
     virtual void send(const uint8_t* data, const uint32_t len) = 0;
     virtual WSString readLine() = 0;
-    virtual void read(uint8_t* buffer, const uint32_t len) = 0;
+    virtual uint32_t read(uint8_t* buffer, const uint32_t len) = 0;
     virtual bool connect(const WSString& host, int port) = 0;
     virtual ~TcpClient() {}
   };
--- src/tiny_websockets/ws_config_defs.hpp
+++ src/tiny_websockets/ws_config_defs.hpp
@@ -1,3 +1,4 @@
 #pragma once
 
 #define _WS_CONFIG_NO_TRUE_RANDOMNESS
+#define _WS_BUFFER_SIZE 512
